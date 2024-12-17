8dc647661ddb6295371d25556333c179ad91b4ab
Priscila-Soares <priscila.soares@aluno.faculdadeimpacta.com.br>
2024-12-17 13:28:20 -0300

    Enviando as classes model, controller e repository de série

--- src/main/java/com/gen/genFit/model/Serie.java
+++ src/main/java/com/gen/genFit/model/Serie.java
@@ -1,5 +1,74 @@
 package com.gen.genFit.model;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_serie")
 public class Serie {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //@ManyToOne
+    //@JoinColumn(name = "usuario_id", nullable = false)
+    private Usuario usuario;
+
+    //@ManyToOne
+    //@JoinColumn(name = "exercicio_id", nullable = false)
+    private Exercicio exercicio;
+
+    @Column(nullable = false, length = 255)
+    private String descricao;
+
+    @Column(nullable = false, length = 100)
+    private String periodo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Exercicio getExercicio() {
+        return exercicio;
+    }
+
+    public void setExercicio(Exercicio exercicio) {
+        this.exercicio = exercicio;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getPeriodo() {
+        return periodo;
+    }
+
+    public void setPeriodo(String periodo) {
+        this.periodo = periodo;
+    }
 }
--- src/main/java/com/gen/genFit/repository/SerieRepository.java
+++ src/main/java/com/gen/genFit/repository/SerieRepository.java
@@ -1,5 +1,11 @@
 package com.gen.genFit.repository;
 
-public class SerieRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.gen.genFit.model.Serie;
+
+@Repository
+public interface SerieRepository extends JpaRepository<Serie, Long> {
 }
+
